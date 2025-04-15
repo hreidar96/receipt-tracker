ef23aba5207c2298fe8062cb8075a948c437a8bc
Hreidar <hreidar220@gmail.com>
2025-04-15 20:07:31 +0000

    landing-page

--- actions/getTemporaryAccessToken.ts
+++ actions/getTemporaryAccessToken.ts
@@ -7,3 +7,25 @@ import { SchematicClient } from "@schematichq/schematic-typescript-node";
 const apiKey = process.env.SCHEMATIC_API_KEY;
 const client = new SchematicClient(apiKey);
 
+// Get a temporary access token for the current user
+export async function getTemporaryAccessToken() {
+    console.log("Getting temporary access token");
+    const user = await currentUser();
+
+    if (!user) {
+        console.log("User not found, retuning null");
+        return null;
+    }
+
+    console.log(`Issuing temporary access token for user ${user.id}`);
+    const resp = await client.accesstokens.issueTemporaryAccessToken({
+        resourceType: "company",
+        lookup: { id: user.id }, // The lookup will vary depending on how you have configured your company keys
+    });
+
+    console.log(
+        "Token response received",
+        resp.data ? "Token received" : "No token in response", 
+    )
+    return resp.data?.token;
+}
